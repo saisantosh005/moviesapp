0beaa13f3a0578de5a7178e14df62887b611a916
Santosh <saisantosh005@gmail.com>
2021-09-01 12:15:02 +0000

    add BannerSection to Home

--- src/components/HomeRoute/Home/Home.js
+++ src/components/HomeRoute/Home/Home.js
@@ -2,10 +2,33 @@ import {Component} from 'react'
 import Cookies from 'js-cookie'
 
 import Header from '../../Header/Header'
-import {HomeMainContainer} from './styledComponent'
+import {
+  HomeMainContainer,
+  BannerSectionContainer,
+  DetailsContainer,
+  BannerDescription,
+  BannerMainHeading,
+  BannerSectionButton,
+  ShadowDiv,
+  TrendingSectionContainer,
+} from './styledComponent'
 
 class Home extends Component {
   state = {}
+
+  renderBannerSection = () => (
+    <BannerSectionContainer>
+      <DetailsContainer>
+        <BannerMainHeading>Super Man</BannerMainHeading>
+        <BannerDescription>
+          Superman is a fictional superhero who first appeared in American comic
+          books published by DC Comics.
+        </BannerDescription>
+        <BannerSectionButton>Play</BannerSectionButton>
+      </DetailsContainer>
+      <ShadowDiv />
+    </BannerSectionContainer>
+  )
 
   render() {
     // Cookies.set('jwt_token', responseData, {expires: 1})
@@ -14,6 +37,8 @@ class Home extends Component {
     return (
       <HomeMainContainer>
         <Header />
+        {this.renderBannerSection()}
+        <TrendingSectionContainer>1</TrendingSectionContainer>
       </HomeMainContainer>
     )
   }
