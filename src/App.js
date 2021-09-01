bc879e7dff9ade4a66ff12eede080ba0178ac8c8
Santosh <saisantosh005@gmail.com>
2021-09-01 05:51:52 +0000

    adds Procted Route in app.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import {BrowserRouter, Switch, Route} from 'react-router-dom'
 import LoginForm from './components/Authentication/LoginForm/LoginForm'
-
+import Home from './components/HomeRoute/Home/Home'
+import ProtectedRoute from './components/ProtectedRoute/ProtectedRoute'
 import './App.css'
 
 const App = () => (
   <div>
     <BrowserRouter>
       <Switch>
-        <Route exact path="/" component={LoginForm} />
+        <Route exact path="/login" component={LoginForm} />
+        <ProtectedRoute exact path="/" component={Home} />
       </Switch>
     </BrowserRouter>
   </div>
